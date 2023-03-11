82c2cbe2eaf8b17fb60c21e9c684676e0a93b25c
talithaprnst <talithaprnst@gmail.com>
2023-03-11 15:29:25 +0700

    menambahkan NIM

--- try1.py
+++ try1.py
@@ -1,4 +1,6 @@
 nama_depan = "Talitha"
 nama_belakang = "Pranastiti"
+nim = "I0320099"
 
-print(nama_depan, nama_belakang)
+print("Namaku adalah", nama_depan, nama_belakang)
+print("dengan NIM", nim)
